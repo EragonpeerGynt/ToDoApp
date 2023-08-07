e0970af4ed3a1939a960ba878ab744794613ab3e
EragonpeerGynt <EragonpeerGynt@users.noreply.github.com>
2023-08-07 12:41:12 +0200

    finished with descriptions for swagger

--- ToDoApp/ToDoApp/Controllers/TaskController.cs
+++ ToDoApp/ToDoApp/Controllers/TaskController.cs
@@ -19,13 +19,38 @@ namespace ToDoApp.Controllers
             _taskManager = (TaskManager)taskManager;
         }
 
+        /// <summary>
+        /// Endpoint namenjen pridobivanju ToDo nalog iz zaledne baze.
+        /// </summary>
+        /// <param name="id">Id po katerem iščemo med ToDo nalogami</param>
+        /// <param name="createdfrom">Iskanje ToDo nalog narejenih po vnešenem datumu</param>
+        /// <param name="createdto">Iskanje ToDo nalog narejenih pred vnešenim datumom</param>
+        /// <param name="completed">Iskanje ToDo nalog, ki so v specifičnem statusu opravljenosti</param>
+        /// <param name="title">Naslov po katerem iščemo med ToDo nalogami</param>
+        /// <returns>Seznam vseh ToDo nalog, ki ustrezajo specificiranim pogojem</returns>
         [HttpGet]
+        [ProducesResponseType(typeof(List<RetrieveTaskResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         public IActionResult Id([FromQuery]int? id, [FromQuery]DateTime? createdfrom, [FromQuery]DateTime? createdto, [FromQuery]bool? completed, [FromQuery]string? title)
         {
-            return Ok(_taskManager.RetrieveTasks(id, createdfrom, createdto, completed, title));
+            try
+            {
+                return Ok(_taskManager.RetrieveTasks(id, createdfrom, createdto, completed, title));
+            }
+            catch (Exception ex) 
+            { 
+                return BadRequest(ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Endpoint namenjen ustvarjanju novih ToDo nalog.
+        /// </summary>
+        /// <param name="task">Objekt v katerem so shranjeni podatki za ustvarjanje nove ToDo naloge</param>
+        /// <returns></returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         public IActionResult Create([FromBody]CreateTaskRequest task)
         {
             CreateResponse response = _taskManager.CreateTask(task);
@@ -39,7 +64,15 @@ namespace ToDoApp.Controllers
             }
         }
 
+        /// <summary>
+        /// Endpoint namenjen posodabljanu obstoječih ToDo nalog.
+        /// </summary>
+        /// <param name="id">Id ToDo naloge, ki jo želimo posodobiti</param>
+        /// <param name="task">Objekt v katerem so shranjeni podatki za posodobitev obstoječe ToDo naloge</param>
+        /// <returns></returns>
         [HttpPatch]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         [Route("id/{id}")]
         public IActionResult Update([Required][FromRoute]int id, [FromBody]UpdateTaskRequest task)
         {
@@ -54,7 +87,14 @@ namespace ToDoApp.Controllers
             }
         }
 
+        /// <summary>
+        /// Endpoint namenjen brisanju obstoječih ToDo nalog.
+        /// </summary>
+        /// <param name="id">Id ToDo naloge, ki jo želimo izbrisati</param>
+        /// <returns></returns>
         [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         [Route("id/{id}")]
         public IActionResult Delete([Required][FromRoute]int id)
         {
